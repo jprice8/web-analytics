a3f0716826d9fb17e36385fef2ed2c5d3dce666a
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-11-03 19:08:02 -0500

    Set up project and environment variables. Config MySQL.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"database/sql"
@@ -7,7 +7,41 @@ import (
 	"os"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB 
 
+type View struct {
+	ID 			int64 
+	ProjectID 	int64 
+	Url 		string
+}
+
+func main() {
+
+	// Load .env file from path
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalf("Error loading .env file.")
+	}
+
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: "analytics",
+	}
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
